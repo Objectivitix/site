21967b221fbaedb435d27b825829e5788323b381
Objectivitix <79152594+Objectivitix@users.noreply.github.com>
2021-07-02 15:33:03 -0300

    Optimize theme toggling in JS

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 var rootStyles = document.querySelector(":root").style;
+var theme = "light";
 
 function toggleDarkTheme() {
-    rootStyles.setProperty("--primary-color", "#ffffff");
-    rootStyles.setProperty("--secondary-color", "#000000");
-    rootStyles.setProperty("--bg-gradient-color-one", "#3c5502");
-    rootStyles.setProperty("--bg-gradient-color-two", "#114618");
-    rootStyles.setProperty("--link-color", "#9944ee");
+    if (theme === "light") {
+        theme = "dark";
+
+        rootStyles.setProperty("--primary-color", "#ffffff");
+        rootStyles.setProperty("--secondary-color", "#000000");
+        rootStyles.setProperty("--bg-gradient-color-one", "#3c5502");
+        rootStyles.setProperty("--bg-gradient-color-two", "#114618");
+        rootStyles.setProperty("--link-color", "#9944ee");
+    }
 }
 
 function toggleLightTheme() {
-    rootStyles.setProperty("--primary-color", "#000000");
-    rootStyles.setProperty("--secondary-color", "#ffffff");
-    rootStyles.setProperty("--bg-gradient-color-one", "#d4fc79");
-    rootStyles.setProperty("--bg-gradient-color-two", "#96e6a1");
-    rootStyles.setProperty("--link-color", "#663399");
+    if (theme === "dark") {
+        theme = "light";
+
+        rootStyles.setProperty("--primary-color", "#000000");
+        rootStyles.setProperty("--secondary-color", "#ffffff");
+        rootStyles.setProperty("--bg-gradient-color-one", "#d4fc79");
+        rootStyles.setProperty("--bg-gradient-color-two", "#96e6a1");
+        rootStyles.setProperty("--link-color", "#663399");
+    }
 }
